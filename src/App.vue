48c85d976e2c856fc54344ae236bb3c7edcb3cc2
Daniel Kelly <me@danielkelly.io>
2021-08-09 15:23:33 -0500

    6. reactive data without refs

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,20 @@
   <h1>{{ name }}</h1>
   <input type="text" v-model="name" />
   <button @click="placeOrder">Place Order</button>
-  <YummyMeal name="Hamburger 🍔" :price="5" @addToCart="addItemToCart" />
+  <YummyMeal :name="meal.name" :price="meal.price" @addToCart="addItemToCart" />
 </template>
 
 <script>
 import YummyMeal from "./components/YummyMeal.vue";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 export default {
   components: { YummyMeal },
   setup() {
     const name = ref("The Snazzy Burger");
+    const meal = reactive({ name: "Hamburger 🍔", price: 5 });
     const placeOrder = () => alert("Your order has been placed!");
     const addItemToCart = (item) => alert(`One ${item} add to the cart`);
-    return { name, placeOrder, addItemToCart };
+    return { name, placeOrder, addItemToCart, meal };
   },
 };
 </script>
