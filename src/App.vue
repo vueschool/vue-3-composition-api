e140ba32f68963b3c059c41a0bc5d94accf9386c
Daniel Kelly <me@danielkelly.io>
2021-08-09 14:50:36 -0500

    5. reactive primitives with refs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>{{ name }}</h1>
+  <input type="text" v-model="name" />
   <button @click="placeOrder">Place Order</button>
   <YummyMeal name="Hamburger 🍔" :price="5" @addToCart="addItemToCart" />
 </template>
 
 <script>
 import YummyMeal from "./components/YummyMeal.vue";
+import { ref } from "vue";
 export default {
   components: { YummyMeal },
   setup() {
-    const name = "The Snazzy Burger";
+    const name = ref("The Snazzy Burger");
     const placeOrder = () => alert("Your order has been placed!");
     const addItemToCart = (item) => alert(`One ${item} add to the cart`);
     return { name, placeOrder, addItemToCart };
