91fb52147d7784e30ee923d19d64f7b1ac9d6826
Daniel Kelly <me@danielkelly.io>
2021-09-08 11:35:06 -0500

    14. overview of example blog

---
+++ src/views/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="post && user">
+    <h1 class="text-3xl">{{ post.title }}</h1>
+    <div class="text-gray-500 mb-10">by {{ user.name }}</div>
+    <div>{{ post.body }}</div>
+  </div>
+</template>
+<script setup>
+const post = {
+  title:
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto",
+};
+const user = {
+  name: "Leanne Graham",
+};
+</script>
